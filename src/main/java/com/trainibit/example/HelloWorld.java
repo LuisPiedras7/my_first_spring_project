42d65f811b42ff6bc739ace5c0d3ff05f4a3603e
LuisPiedras7 <piedrasluis7@gmail.com>
2025-08-31 21:49:40 -0600

    Agregar clase User

--- src/main/java/com/trainibit/example/HelloWorld.java
+++ src/main/java/com/trainibit/example/HelloWorld.java
@@ -2,14 +2,21 @@ package com.trainibit.example;
 
 public class HelloWorld {
     private String message;
+    private User user; // Nuevo atributo
 
-    // Setter method for dependency injection
     public void setMessage(String message) {
         this.message = message;
     }
 
-    // Method to print the message
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public void printMessage() {
         System.out.println("Message: " + message);
+        if (user != null) {
+            System.out.println("User: " + user.getName() + " " + user.getLastName()
+                    + " | Birthdate: " + user.getBirthDate());
+        }
     }
 }
